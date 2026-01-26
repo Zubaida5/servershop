7f5ae48aa5bdb76fba1aa4e51dc46e3a9c8345b1
Mohammed Adel Seirafi <maszzzzy@gmail.com>
2026-01-26 14:44:05 +0300

    init

---
+++ swagger/swagger.js
@@ -0,0 +1,82 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const { signUp } = require('./routes/auth');
+const { User, updateMe, createUser } = require('./routes/users');
+const {
+  DuplicateEmail,
+  Error,
+  Forbidden,
+  NotFound,
+  Unauthorized,
+} = require('./components');
+
+const options = {
+  url: '',
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'REST API Docs',
+      version: '1.0.0',
+      description:
+        'This is an API simpel Auth made with Express and documented with Swagger',
+    },
+    servers: [
+      {
+        url: 'http://localhost:7000/api/v1.0.0',
+        description: 'Development server',
+      },
+    ],
+    components: {
+      schemas: {
+        signUp,
+        createUser,
+        updateMe,
+        User,
+        Error,
+      },
+      securitySchemes: {
+        Bearer: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          description: 'Enter the token : abcde12345".',
+        },
+      },
+      responses: {
+        DuplicateEmail,
+        Forbidden,
+        NotFound,
+        Unauthorized,
+        201: {
+          description: 'created',
+        },
+        200: {
+          description: 'ok',
+        },
+        204: {
+          description: 'No content',
+        },
+        400: {
+          description: 'Bad request',
+        },
+        401: {
+          description: 'Unauthorized',
+        },
+        403: {
+          description: 'Forbidden',
+        },
+        404: {
+          description: 'Not found',
+        },
+        413: {
+          description: 'Payload Too Large',
+        },
+        500: {
+          description: 'Internal server error',
+        },
+      },
+    },
+  },
+  apis: ['./swagger/routes/*.js'],
+};
+const swaggerSpec = swaggerJsDoc(options);
+module.exports = swaggerSpec;
